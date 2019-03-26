69bb3628cb1fd1ddc8083628f27425494fde328f
Jake Caccamo <caccamo_jacob@bah.com>
2019-03-25 22:23:50 -0400

    Adding Routing to BB

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Order.css';
+
+const order = (props) => {
+    return (
+        <div className={classes.Order}>
+            <p>Ingreidents</p>
+        </div>
+    );
+}
+
+export default order;
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import axios from '../../../axios-orders.js';
+
+class ContactData extends Component {
+    state = {
+        name: "",
+        email: "",
+        address: {
+            street: "",
+            postalCode: ""
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: "JakeCAC",
+                address: {
+                    street: '123 Street',
+                    zipCode: '45343',
+                    country: 'US'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: "fastest"
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading: false});
+                this.props.history.push('/');
+            }).catch(error => {
+                this.setState({loading: false});
+            });
+    }
+
+    render() {
+        let form = (<form>
+            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+            <input className={classes.Input} type="text" name="email" placeholder="Your Email" />
+            <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
+            <input className={classes.Input} type="text" name="postal" placeholder="Your Zip Code" />
+            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />
+        }
+
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Information</h4>
+                {form}
+            </div>
+        );
+    }
+}
+
+export default ContactData;
